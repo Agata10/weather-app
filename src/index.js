bbd937f479365ee115ff4b870e24afe408b9d97a
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-01-16 15:44:45 -0700

    Set up project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+async function getDataWeather() {
+  const weather = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=London&aqi=no`
+  );
+  const response = weather.json();
+  console.log(response);
+}
