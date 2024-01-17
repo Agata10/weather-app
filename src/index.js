bc80a86abac341ecb0188511d42362b70e91fba0
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-01-16 18:42:44 -0700

    Add API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 async function getDataWeather() {
-  const weather = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=London&aqi=no`
-  );
-  const response = weather.json();
-  console.log(response);
+  try {
+    const weather = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=London&aqi=no`
+    );
+    const response = await weather.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-getDataWeather();
+function setWeather(response) {
+  const locationWeather = {
+    name: response.location.name,
+    country: response.location.country,
+    condition: response.current.condition.text,
+    condition_icon: response.current.condition.icon,
+    temp_c: response.current.condition.temp_c,
+    temp_f: response.current.condition.tem_f,
+    humidity: response.current.condition.humidity,
+    wind_mph: response.current.condition.wind_mph,
+  };
+  console.log(locationWeather);
+}
+
+getDataWeather().then((response) => setWeather(response));
